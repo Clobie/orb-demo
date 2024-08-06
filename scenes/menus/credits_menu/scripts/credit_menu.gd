d8c3f2d67e79b7425e19ea3f85640d3dbcab9795
Clobie <calebtalexander@gmail.com>
2024-08-06 17:34:50 -0400

    load example level on singleplayer button

--- scenes/menus/credits_menu/scripts/credit_menu.gd
+++ scenes/menus/credits_menu/scripts/credit_menu.gd
@@ -6,4 +6,4 @@ func _ready():
 	pass
 
 func _on_button_back_pressed():
-	get_tree().change_scene_to_file("res://scenes/menus/start_menu/start_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/menus/start_menu/startmenu.tscn")
--- scenes/worlds/example_level/scripts/room1.gd
+++ scenes/worlds/example_level/scripts/room1.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
+@onready var directional_light_2d_darkness = $Lights/DirectionalLight2D_Darkness
+
+
 func _ready() -> void:
-	pass
+	directional_light_2d_darkness.visible = true
 
 func _process(delta: float) -> void:
 	pass
